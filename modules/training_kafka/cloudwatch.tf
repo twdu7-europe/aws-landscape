8ddff68a39a6aadf4c8aa830cc20ba514f397121
jmolina91 <javier.m@thoughtworks.com>
2021-08-04 09:03:30 +0200

    7 - Javi/Nadia : Add extra dimensions to cloudwatch alarm

--- modules/training_kafka/cloudwatch.tf
+++ modules/training_kafka/cloudwatch.tf
@@ -10,6 +10,8 @@ resource "aws_cloudwatch_metric_alarm" "kafka_disk_space_saturation_alarm" {
   alarm_description         = "This metric monitors kafka disk utilization"
   insufficient_data_actions = []
   dimensions = {
-    InstanceId = aws_instance.kafka.id
+    InstanceId = "${aws_instance.kafka.id}"
+    MountPath  = "/"
+    FileSystem = "/dev/xvda1"
   }
 }
