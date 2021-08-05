b67cfa43b9bb8b36e6584539d77fabb59ed5828d
jmolina91 <javier.m@thoughtworks.com>
2021-08-05 12:54:18 +0200

    7 - Javi/Jade : Remove kms key for sns topic

--- modules/training_kafka/sns.tf
+++ modules/training_kafka/sns.tf
@@ -1,7 +1,6 @@
 # SNS topic to send emails with the Alerts
 resource "aws_sns_topic" "kakfa-alarms" {
   name              = "kafka-alarms-topic"
-  kms_master_key_id = "${aws_kms_key.sns_encryption_key.id}"
   delivery_policy   = <<EOF
 {
   "http": {
@@ -25,12 +24,4 @@ EOF
   provisioner "local-exec" {
     command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint ${var.email_alarms} --region ${var.aws_region}"
   }
-}
-
-
-## KMS Key to encrypt the SNS topic (security best practises)
-resource "aws_kms_key" "sns_encryption_key" {
-  description             = "alarms sns topic encryption key"
-  deletion_window_in_days = 30
-  enable_key_rotation     = true
 }
