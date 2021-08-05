b4300201dc37257b8a007cc9129b512b236db8c2
j-forsb <jade.forsberg@thoughtworks.com>
2021-08-05 16:12:54 +0100

    7 - Jade/Javi : update cloudwatch alert to monitor kafka disk space utilization

--- modules/training_kafka/cloudwatch.tf
+++ modules/training_kafka/cloudwatch.tf
@@ -2,15 +2,17 @@ resource "aws_cloudwatch_metric_alarm" "kafka_disk_space_saturation_alarm" {
   alarm_name                = "kafka-disk-space-saturation"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "2"
-  metric_name               = "CPUUtilization"
-  namespace                 = "AWS/EC2"
+  metric_name               = "DiskSpaceUtilization"
+  namespace                 = "System/Linux"
   period                    = "60"
   statistic                 = "Average"
-  threshold                 = "0.2"
+  threshold                 = "20"
   alarm_description         = "This metric monitors kafka disk utilization"
   insufficient_data_actions = []
   dimensions = {
     InstanceId = "${aws_instance.kafka.id}"
+    MountPath = "/"
+    Filesystem = "/dev/xvda1"
   }
   alarm_actions = ["${aws_sns_topic.kakfa-alarms.id}"]
 }
