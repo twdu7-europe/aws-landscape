b37cd4030d1f3bb5c2124223143d39b36e20ff4b
jmolina91 <javier.m@thoughtworks.com>
2021-08-06 13:53:28 +0200

    11 - Javi/Jade: Fix set_kafka_retention_period script

--- scripts/set_kafka_retention_period.sh
+++ scripts/set_kafka_retention_period.sh
@@ -1,8 +1,37 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 
-TRAINING_COHORT=$1
+BASTION_PUBLIC_IP=$1
+TRAINING_COHORT=$2
+
+echo "====Updating SSH Config===="
+
+echo "
+	User ec2-user
+	IdentitiesOnly yes
+	ForwardAgent yes
+	DynamicForward 6789
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
+
+Host emr-master.${TRAINING_COHORT}.training
+    User hadoop
+
+Host *.${TRAINING_COHORT}.training !bastion.${TRAINING_COHORT}.training
+	ForwardAgent yes
+	ProxyCommand ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ec2-user@${BASTION_PUBLIC_IP} -W %h:%p 2>/dev/null
+	User ec2-user
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
+
+Host bastion.${TRAINING_COHORT}.training
+    User ec2-user
+    HostName ${BASTION_PUBLIC_IP}
+    DynamicForward 6789
+" >> ~/.ssh/config
+
+echo "====SSH Config Updated===="
 
 echo "====Updating Kafka Properties===="
 ssh kafka.${TRAINING_COHORT}.training <<EOF
