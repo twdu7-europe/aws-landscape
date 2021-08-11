62a65dd4694a2605b94fa0365a61d33ec2b85739
jmolina91 <javier.m@thoughtworks.com>
2021-08-11 11:41:38 +0200

    javi/mario: create airflow ami

---
+++ images/training_airflow/install_airflow.sh
@@ -0,0 +1,12 @@
+sudo apt-get -y install python3-pip
+pip3 install virtualenv
+virtualenv --no-site-packages venv
+source venv/bin/activate
+pip install apache-airflow['postgres']
+
+# Install cloud watch agent prerequirement
+sudo yum clean all && sudo yum install -y perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https perl-Digest-SHA.x86_64
+curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
+unzip CloudWatchMonitoringScripts-1.2.2.zip && \
+rm CloudWatchMonitoringScripts-1.2.2.zip && \
+(crontab -l 2>/dev/null; echo "*/5 * * * * ~/aws-scripts-mon/mon-put-instance-data.pl --mem-used-incl-cache-buff --mem-util --mem-used --mem-avail --disk-space-used --disk-space-avail --disk-space-util --disk-path=/ --from-cron") | crontab -
